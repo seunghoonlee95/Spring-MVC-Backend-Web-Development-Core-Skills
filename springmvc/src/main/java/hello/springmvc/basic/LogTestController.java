0553dc9758330be40300d418ffd8c09f0aa29fcb
이승훈 사원 은행/카드DX2팀 (seunghoonlee@lgcns.com <seunghoonlee@cns.lgcns.com>
2024-11-12 17:40:49 +0900

    Logging 간단히 알아보기

---
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(LogTestController.class); //or (getClass())
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        //아래로 갈 수록 심각도 높음.
+        log.trace("trace log = {}" , name);
+        log.debug("trace log = {}" , name);
+        log.info("trace log = {}" , name);
+        log.warn("trace log = {}" , name);
+        log.error("trace log = {}" , name);
+
+        return "ok";
+    }
+}
